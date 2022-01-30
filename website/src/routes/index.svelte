88b1db832115e0bb2ca250caf760743bb365d067
Joel <joelhowse@gmail.com>
2022-01-31 12:44:03 +1300

    add website

---
+++ website/src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
+<h1>Stud Manager Home Page</h1>
+
+<p>
+	Welcome to the home page for the stud manager. This is currently only a skeleton website, if you
+	have any issues or questions email <a href="mailto: joelhowse@gmail.com">joelhowse@gmail.com</a>!
+</p>
+
+<a href={`${base}/privacy-policy`}>Privacy Policy</a>
